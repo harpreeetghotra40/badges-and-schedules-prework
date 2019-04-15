130e65b1a1d250558dd61b3aab3494e617911323
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 21:23:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,15 +10,7 @@ def batch_badge_creator(array_of_names)
   return newArray
 end
 
-def assign_rooms(array_of_names)
-  iterator = 1
-  newArray = []
-  array_of_names.each do |name|
-    newArray << "Hello, #{name}! You'll be assigned to room #{iterator}!"
-    iterator++
-  end
-  return newArray
-end
+
 
 def printer
 
